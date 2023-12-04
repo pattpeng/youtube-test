b432f4b7679252426f220f30aaf724688d8486ab
Patt Peng <patt.peng@gmail.com>
2023-12-04 13:55:53 +0700

    Add build files and update Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/youtube-test/'
+  base: '/youtube-test/',
+  "homepage": "https://pattpeng.github.io/youtube-test/",
   plugins: [react()],
 })
