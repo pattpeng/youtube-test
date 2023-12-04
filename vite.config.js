941f4203dde6d904bb7519cca4c2b35345961b40
Patt Peng <patt.peng@gmail.com>
2023-12-04 14:04:00 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/youtube-test/',
-  "homepage": "https://pattpeng.github.io/youtube-test/",
   plugins: [react()],
-})
+});
