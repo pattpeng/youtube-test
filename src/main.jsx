1d595c833ea1a6e878565ecf2b2f44d38f6edc4e
Patt Peng <patt.peng@gmail.com>
2023-12-04 21:10:40 +0700

    add3

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
+  <Router basename='/index.html'>
     <App />
+  </Router>
 
 
 )
